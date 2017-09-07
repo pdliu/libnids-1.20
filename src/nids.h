b8108edecb81ce1d0d74dcf66e8c094f12a5bc1d
pdliu <liupeidong@buaa.edu.cn>
2017-09-07 15:23:44 +0800

    comments from https://linux.die.net/man/3/libnids

--- src/nids.h
+++ src/nids.h
@@ -30,11 +30,13 @@ enum
   NIDS_WARN_TCP_BADFLAGS
 };
 
-#define NIDS_JUST_EST 1
-#define NIDS_DATA 2
-#define NIDS_CLOSE 3
-#define NIDS_RESET 4
-#define NIDS_TIMED_OUT 5
+#define NIDS_JUST_EST 1   // Connection just established. Connection parameters in the addr structure are available for inspection. 
+                          // If the connection is interesting, the TCP callback function may specify which data it wishes to receive in the future
+                          // by setting non-zero values for the collect or collect_urg variables in the appropriate client or server half_stream structure members.
+#define NIDS_DATA 2       // New data has arrived on a connection. The half_stream structures contain buffers of data.
+#define NIDS_CLOSE 3      // *** Connection has closed. The TCP callback  
+#define NIDS_RESET 4      // *** function should free any resources it
+#define NIDS_TIMED_OUT 5  // *** may have allocated for this connection.
 #define NIDS_EXITING   6	/* nids is exiting; last chance to get data */
 
 #define NIDS_DO_CHKSUM  0
@@ -63,7 +65,7 @@ struct half_stream
   int count;          // The number of bytes appended to data since the creation of the connection.  
   int count_new;      // The number of bytes appended to data since the last invocation of the TCP callback 
                       // function (if 0, no new data arrived).
-  int bufsize;        // 
+  int bufsize;
   int rmem_alloc;
 
   int urg_count;
@@ -71,8 +73,9 @@ struct half_stream
   u_int seq;
   u_int ack_seq;
   u_int first_data_seq;
-  u_char urgdata;
-  u_char count_new_urg;
+  u_char urgdata;      // One-byte buffer for urgent data.  
+  u_char count_new_urg;// The number of bytes appended to urgdata since the last invocation of the TCP callback
+                       // function (if 0, no new urgent data arrived).
   u_char urg_seen;
   u_int urg_ptr;
   u_short window;
@@ -87,7 +90,8 @@ struct half_stream
 struct tcp_stream
 {
   struct tuple4 addr;
-  char nids_state;
+  char nids_state;  // The value of the nids_state field provides information about the state of the TCP connection, 
+                    // to be used by the TCP callback function: NIDS_JUST_EST,NIDS_DATA,NIDS_CLOSE,NIDS_RESET,NIDS_TIMED_OUT
   struct lurker_node *listeners;
   struct half_stream client;
   struct half_stream server;
@@ -145,13 +149,30 @@ void nids_register_ip (void (*));      // registers a user-defined callback func
                                        // packets validated and reassembled by libnids.
 void nids_register_tcp (void (*));     // registers a user-defined callback function to process TCP 
                                        // streams validated and reassembled by libnids. 
-void nids_register_udp (void (*));     // 
-void nids_killtcp (struct tcp_stream *);
-void nids_discard (struct tcp_stream *, int);
-void nids_run (void);
-int nids_getfd (void);
-int nids_dispatch (int);
-int nids_next (void);
+                                       // The 'param' pointer passed by libnids as argument to the TCP callback function may be set to
+                                       // save a pointer to user-defined connection-specific data to pass to subsequent invocations of
+                                       // the TCP callback function (ex. the current working directory for an FTP control connection, etc.).
+                                       // The 'user' pointer in the tcp_stream structure has the same purpose except it is global to
+                                       // the stream, whereas the 'param' pointer is different from one callback function to
+                                       // the other even though they were called for the same stream.
+void nids_register_udp (void (*));      
+void nids_killtcp (struct tcp_stream *);// tears down the specified TCP connection with symmetric RST packets between client and server.
+void nids_discard (struct tcp_stream *, int); // may be called from the TCP callback function to specify the number of bytes to 
+                                              // discard from the beginning of the 'data' buffer (updating the 'offset' value 
+                                              // accordingly) after the TCP callback function exits. Otherwise, the new 
+                                              // data (totalling 'count_new' bytes) will be discarded by default.
+void nids_run (void);    // starts the packet-driven application, reading packets in an endless loop, and invoking registered 
+                         // callback functions to handle new data as it arrives. This function does not return.
+int nids_getfd (void);   // may be used by an application sleeping in 'select'(package?) to snoop for a socket file descriptor
+                         // present in the read fd_set. Returns the file descriptor on success, -1 on failure (in which case 
+                         // 'nids_errbuf' contains an appropriate error message).
+int nids_dispatch (int); // attempts to process 'cnt' packets before returning, with a cnt of -1 understood as all packets 
+                         // available in one pcap buffer, or all packets in a file when reading offline. 
+                         // On success, returns the count of packets processed, which may be zero upon EOF (offline read) or 
+                         // upon hitting pcap_timeout (if supported by your platform). 
+                         // On failure, returns -1, putting an appropriate error message in 'nids_errbuf'.
+int nids_next (void);    // processes the next available packet before returning. Returns 1 on success, 0 if no packet was 
+                         // processed, setting 'nids_errbuf' appropriately if an error prevented packet processing.
 
 extern struct nids_prm nids_params;
 extern char *nids_warnings[];
@@ -164,7 +185,14 @@ struct nids_chksum_ctl {
 	u_int action;
 	u_int reserved;
 };
-extern void nids_register_chksum_ctl(struct nids_chksum_ctl *, int);
+extern void nids_register_chksum_ctl(struct nids_chksum_ctl *, int); // takes as arguments an array of struct nids_chksum_ctl 
+                                                                     // elements and the number of elements in the array.
+// Internal checksumming functions will first check elements of this array one by one, and 
+// if the source ip SRCIP of the current packet satisfies condition 
+//      ((SRCIP&chksum_ctl_array[i].mask)==chksum_ctl_array[i].netaddr),
+// then if the action field is NIDS_DO_CHKSUM, the packet will be checksummed; 
+//      if the action field is NIDS_DONT_CHKSUM, the packet will not be checksummed. 
+// If the packet matches none of the array elements, the default action is to perform checksumming.
 
 #define print(A) fprintf(stderr, A);\
                  fprintf(stderr,"\n");
